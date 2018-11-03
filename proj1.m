d054059221291a906a3b82b792523422ca56d0e3
JoaoCarv <joao.franciscocarv@gmail.com>
2018-11-03 14:20:42 +0000

    added template

---
+++ proj1.m
@@ -0,0 +1,30 @@
+
+%-------------------------Otimization and Algorithms----------------------%
+%-------------------------------Projet - Part 1 --------------------------%
+%-------------------------------------------------------------------------%
+
+
+% --------Work by:------%
+% Catia Fortunato
+% Maria Inês Diegues
+% Joao Carvalho
+% -------Supervision by------%
+% Profesor João Xavier
+
+
+%%
+clear all;
+close all;
+
+%% --------------------------Variation A
+
+%----------Constants:
+T=80;
+pi=[0,5]; %initial position
+pf=[15,-15]; %final position
+
+plot(lin
+
+%----------Cost Function:
+%1st term (3rd wish)
+E=[1 0 0 0; 0 1 0 0};

5c85708a9825eee135723ef271d413d80972b573
Cátia Fortunato <catiafortunato35@gmail.com>
2018-11-22 16:54:19 +0000

    corrected X

--- part2/newton/newton_task6.m
+++ part2/newton/newton_task6.m
@@ -1,18 +1,19 @@
 %% Task 6: 
 
 % Solve newton method for data set 1
-
+load data1.mat
 s=[-1,-1];
-r0=0;
+r=0;
 epsilon=10^-6;
 alpha_hat=1;
 gama=10^-4;
 beta=0.5;
-K=length(X);       
+K=size(X,2);       
+
+P=[s r];
+new_x = [X;ones(1,size(X,2))];
 
 % Vectorized implementation of the gradiente
-Fs=(1/K)*(-sum(Y.*X,2)+sum(X.*exp(s*X-r0)./(1+exp(s*X-r0)),2));
-Fr=(1/K)*(sum(Y)+sum(-(exp(s*X-r0))./(1+exp(s*X-r0))));
-g=[Fs;Fr];
+g=sum(new_x.*exp(P*new_x)./(1+exp(P*new_x)),2)- sum(new_x.*Y,2);
 
 % Vectorized implementation of Hessian 
